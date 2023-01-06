b3a64bfa811035eeb431f308835f681eea51c434
Jhonny Chipana <jhonnyeddy.22@gmail.com>
2023-01-05 22:23:37 -0400

    add db config

--- config.js
+++ config.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const config = {
     port: process.env.PORT || 6640,
     database: {
+        connectionLimit: 1000,
+        connectionTimeout: 60 * 60 *1000,
+        acquireTimeout: 60 * 60 *1000,
+        timeout: 60 * 60 *1000,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         host: process.env.DB_HOST || 'localhost',
